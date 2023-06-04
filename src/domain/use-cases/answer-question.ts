5bed0f62649f1c378dabdfeaeeb9dc02851f04ff
Gabriel Borges <contato@gbdev.me>
2023-06-03 22:34:01 -0300

    Fundamentos do DDD

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,27 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
+import { Answer } from "../entities/answer";
+import { AnswersRepository } from "../repositories/answers-repository";
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
+export class AnswerQuestionUseCase {
+  constructor(
+    private answersRepository: AnswersRepository
+  ){}
+
+  async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    })
+
+    await this.answersRepository.create(answer)
+
+    return answer
+  }
+}
